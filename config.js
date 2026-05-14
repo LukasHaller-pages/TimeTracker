5acd7333e8dac62a48ad5b6574101a4fd753b5ad
22997199 <lukas.haller@edu.htl-klu.at>
2026-05-14 21:07:49 +0200

    Site up

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    GITLAB_URL: "https://git.htl-klu.at",
+    GITLAB_TOKEN: "HTLKLUe92V4ouxlGPFNqrKlu1igW86MQp1OjFtaQk.01.0z1a47rhk",
+    PROJECT_ID: "12729",
+    BRANCH: "main",
+    FILE_PATH: "times.csv"
+};
